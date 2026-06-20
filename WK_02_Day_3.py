5755496a3509a42dd3d462bd7ea20e790a68e4e8
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-20 21:09:14 +0400

    Working Dictionaries

---
+++ WK_02_Day_3.py
@@ -0,0 +1,58 @@
+# Working Dictionaries
+# AI ENGINEERING PROFILE
+# engineer = {
+#     "name":"John",
+#     "role":"AI Engineer",
+#     "Experience":3,
+#     "country":"United Kingdom"
+# }
+
+# print(engineer)
+
+# Accessing Values
+
+# engineer = {
+#     "name":"John",
+#     "role":"AI Engineer",
+#     "experience":3,
+#     "country":"United Kingdom"
+# }
+
+# print(engineer["name"])
+# print(engineer["role"])
+# print(engineer["experience"])
+
+# Candidate Evaluation
+# candidate = {
+#     "name":"Alice",
+#     "python":90,
+#     "sql":85,
+#     "machine_learning":88
+# }
+# for key in candidate:
+#     print(key,":",candidate[key])
+
+# Student Record
+# student = {
+#     "id":1001,
+#     "name":"David",
+#     "score":92
+# }
+# print(student)
+
+# Project: AI Dataset Record
+# dataset_record = {
+#     "record_id":1,
+#     "customer_name":"James",
+#     "purchase_amount":1500,
+#     "country":"United Kingdom"
+# }
+# print("=" * 40)
+# print("DATASET RECORD")
+# print("=" * 40)
+# for key in dataset_record:
+#     print(key,":",dataset_record[key])
+# EXERCISES
+# Ctreate a dictionary for an employee
+# name, department, salary, country
+# Print all values
