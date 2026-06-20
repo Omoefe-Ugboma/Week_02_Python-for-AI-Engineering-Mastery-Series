cba31d850ddddb28cd7a20ae531887567c79c30d
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-20 16:50:34 +0400

    Week_02_Python for AI Engineering Mastery Series Day 1 and Day 2

---
+++ WK_02_Day_1.py
@@ -0,0 +1,56 @@
+# Working with List
+# Candidate Registration System
+
+# print("=" * 50)
+# print("AI CANDIDATE REGISTRATION SYSTEM")
+# print("=" * 50)
+
+# candidates = ["John","Mary","David","Sarah","Michael"]
+# print("\nREGISTERED CANDIDATES\n")
+
+# for candidate in candidates:
+#     print(candidate)
+    
+# print("\nTotal Candidates:", len(candidates))
+
+# Employee Database
+# employees = ["Alice","Bob","Charlie","David","Emma"]
+
+# print("EMPLOYEE DATABASE")
+# print("-" * 30)
+
+# for employee in employees:
+#     print(employee)
+
+# Accessing List items
+# products = ["Laptop","Phone","Tablet","Monitor"]
+# print(products[0])
+# print(products[1])
+# print(products[2])
+# print(products[3])
+
+# Modifying Lists
+# products = ["Laptop","Phone","Tablet","Monitor"]
+
+# print(products)
+# products[1] = "iPhone"
+# print(products)
+
+# AI TRAINING DATASET
+# scores = [85,72,95,88,90,67,78]
+# print("AI TRAINING DATASET")
+# print("-" * 30)
+
+# for score in scores:
+#     print(score)
+
+# print()
+
+# print("Highest Score:", max(scores))
+# print("Lowest Score: ", min(scores))
+# print("Average Score: ", sum(scores)/len(scores))
+
+# Create a list of 5 Countries
+# Create a list of 5 AI tools
+# Print each item using a loop
+# Print total number of items
