f43f65fe75cb885e5c23d092fcd48bf56a94ab80
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-20 18:28:40 +0400

    Working with tuples in python

---
+++ WK_02_Day_2.py
@@ -0,0 +1,47 @@
+# Working with TUPLES
+# Example 1: AI MODEL CONFIGURATION
+# model_config = (
+#     "GPT-5",
+#     8192,
+#     "English",
+#     True
+# )
+# print(model_config)
+
+# Example 2: CLOUD SERVER CONFIGURATION
+# server_config = (
+#     "AWS",
+#     "Ubuntu",
+#     "16GB RAM",
+#     "8 CPUs"
+# )
+# for item in server_config:
+#     print(item)
+ 
+# P ROJECT : AI PROJECT METADATA
+
+# project = (
+#     "AI Recruitment System",
+#     "Python",
+#     "FastAPI",
+#     "PostgreSQL",
+#     "OpenAI"
+# )
+
+# print("=" * 40)
+
+# print("PROJECT DETAILS")
+
+# print("=" * 40)
+
+# for item in project:
+#     print(item)
+
+# EXERCISES
+# Create a tuple containig:
+#   Project Name
+# Language
+# Database
+# Cloud
+
+# Print all values
