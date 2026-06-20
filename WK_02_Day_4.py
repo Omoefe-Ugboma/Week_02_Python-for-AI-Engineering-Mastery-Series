7fa52295478600286fbe7de84ac04ffb3843cdfd
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-20 21:43:33 +0400

    Working Set

---
+++ WK_02_Day_4.py
@@ -0,0 +1,48 @@
+# Working Set
+# Example 1: Remove Duplicate Emails
+# emails = [
+#     "john@gmail.com",
+#     "mary@gmail.com",
+#     "john@gmail.com",
+#     "alice@gmail.com",
+#     "mary@gmail.com"
+# ]
+# unique_emails = set(emails)
+# print(unique_emails)
+
+# Example 2: Attendance System
+
+# attendance = [
+#     "John",
+#     "Mary",
+#     "Sarah",
+#     "John",
+#     "Alice",
+#     "David",
+#     "Alice",
+#     "Mary"
+# ]
+
+# unique_attend = set(attendance)
+# print(unique_attend)
+
+# Project: AI DATASET CLEANER
+
+# labels = [
+#     "Positive",
+#     "Negative",
+#     "Positive",
+#     "Neutral",
+#     "Negative"
+# ]
+# clean_labels = set(labels)
+# print("ORIGINAL LABELS")
+# print(labels)
+# print()
+# print("UNIQUE LABELS")
+# print(clean_labels)
+
+# Exercises
+# Create a list containing duplicate countries,
+# Convert it to a a set 
+# Print the result
